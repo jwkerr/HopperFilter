40275fb998baa50d53ac0e5659cae51d419a0580
Fruitloopins <49851457+jwkerr@users.noreply.github.com>
2024-07-02 04:45:28 +1000

    Refactor listener registration

--- src/main/java/net/earthmc/hopperfilter/HopperFilter.java
+++ src/main/java/net/earthmc/hopperfilter/HopperFilter.java
@@ -1,19 +1,21 @@
 package net.earthmc.hopperfilter;
 
 import net.earthmc.hopperfilter.listener.InventoryMoveItemListener;
-import org.bukkit.plugin.PluginManager;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class HopperFilter extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        registerListeners();
+        registerListeners(
+                new InventoryMoveItemListener()
+        );
     }
 
-    private void registerListeners() {
-        PluginManager pm = getServer().getPluginManager();
-
-        pm.registerEvents(new InventoryMoveItemListener(), this);
+    private void registerListeners(Listener... listeners) {
+        for (Listener listener : listeners) {
+            getServer().getPluginManager().registerEvents(listener, this);
+        }
     }
 }
